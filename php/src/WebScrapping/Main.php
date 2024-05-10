79731a66833cc0fe71070a15f94342c591d51ec8
AndDev <anddesenvolved@gmail.com>
2024-05-10 09:53:46 -0300

    feat: making the spreadsheet first row

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
-
+use OpenSpout\Common\Entity\Row;
+use OpenSpout\Writer\XLSX\Writer;
+use OpenSpout\Common\Entity\Cell\NumericCell;
+use OpenSpout\Common\Entity\Cell\StringCell;
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -15,9 +18,22 @@ class Main {
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
     $data = (new Scrapper())->scrap($dom);
-
     // Write your logic to save the output file bellow.
-    print_r($data);
+    //Assembling the spreadsheet format
+    $writer = new Writer();
+
+    $writer->openToFile(__DIR__ . '/../../assets/teste.xlsx');
+
+    $headerRow = new Row([
+      new StringCell('ID', null),
+      new StringCell('Title', null),
+      new StringCell('Type', null)
+    ]);
+  for ($i = 1; $i <= 9; $i++) {
+      $headerRow->addCell(new StringCell("Author $i", null));
+      $headerRow->addCell(new StringCell("Author $i Institution", null));
+  }
+  $writer->addRow($headerRow);
   }
 
 }
